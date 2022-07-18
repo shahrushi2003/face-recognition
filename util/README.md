dddba2d826d4be61c97b75b70ff9c64ea0235c2f
Ayush Anand <iamayushanand@gmail.com>
2022-07-18 21:29:30 +0530

    repo cleanup

--- util/README.md
+++
@@ -1,3 +0,0 @@
-# DSFD-Pytorch
-
-Download [weights](https://drive.google.com/file/d/1WeXlNYsM6dMP3xQQELI-4gxhwKUQxc3-/view) and put in face_detection/weights/
--- util/face_detection/layers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .detection import Detect
-from .prior_box import PriorBox, get_prior_boxes
-from .modules import FEM, pa_multibox, mio_module, upsample_product
